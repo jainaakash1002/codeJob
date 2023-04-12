a6c7872eadce4ee64f34c2826fe3bb3d2a749d9b
Aakash Jain <jainaakash1002@gmail.com>
2023-04-13 00:49:58 +0530

    added handlebars and integrated with the jobs route services

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
 const express = require("express");
-// const exphbs = require("express-handlebars");
-// const exphbs = require("express-handlebars");
-
 const { engine } = require("express-handlebars");
-
 const bodyParser = require("body-parser");
 const path = require("path");
 const db = require("./config/database");
 // require("dotenv").config();
 
 db.authenticate()
-  .then(() => console.log("Database connect Mr Aakash"))
-  .catch((err) => console.log("Error: " + err));
+  .then(() => console.log("Database connect Mr Aakash 😎"))
+  .catch((err) => console.log("Error🥲: " + err));
 
 const app = express();
 
 app.engine("handlebars", engine({ extname: ".hbs", defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // app.set("views", "./views");
-// const app = express();
 
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
-app.get("/", (req, res) => res.send("INDEX"));
+// app.get("/", (req, res) => res.send("INDEX"));
+app.get("/", (req, res) => res.render("index", { layout: "landing" }));
 app.use("/jobs", require("./routes/jobs"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 3000;
 
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,62 +1,47 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../config/database");
-// const Jobs = require("../models/Jobs");
-
-// router.get("/", (req, res) => res.send("JOBS"));
-
-// router.get("/", (req, res) =>
-//   Jobs.findAll()
-//     .then((jobs) => {
-//       console.log(jobs);
-//       res.sendStatus(200);
-//     })
-//     .catch((err) => console.log(err))
-// );
-
 const Jobs = require("../models/Jobs");
 
 router.get("/", (req, res) =>
   Jobs.findAll()
     .then((jobs) => {
-      console.log(jobs);
-      res.sendStatus(200);
+      // res.send(jobs);
+      // res.sendStatus(200);
+      const jobsJson = jobs.map((job) => job.toJSON());
+      res.render("jobs", { jobs: jobsJson });
     })
-    .catch((err) => console.log(err))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Server error" });
+    })
 );
 
-//add a job
-
 router.get("/add", (req, res) => {
-  const data = {
-    title: "Looking for a Angular Developer",
-    technologies: "angular, typescript, chakraUi, tailwind",
-    budget: "100000",
-    description:
-      "Hi, i am looking for a angular web developer job and my skills are html, css, tailwing, angular and github. Knows some ui frameworks as well like chakra ui , mui, antD.",
-    contact_email: "happy@gmail.com",
-  };
-
-  let { title, technologies, budget, description, contact_email } = data;
-  Jobs.create({
-    title,
-    technologies,
-    budget,
-    description,
-    contact_email,
-  })
-    .then((jobs) => res.redirect("/jobs"))
-    .catch((err) => console.log(err));
+  res.render("add");
 });
 
-// router.get("/", async (req, res) => {
-//   try {
-//     const jobs = await Jobs.findAll();
-//     res.json(jobs);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ message: "Server error" });
-//   }
+//add a job
+// router.post("/add", (req, res) => {
+//   const data = {
+//     title: "Looking for a JavaScript Developer",
+//     technologies: "javascript, html, css, bootstrap",
+//     budget: "50000",
+//     description:
+//       "Hi, i am looking for a javascript developer job and my skills are html, css and github. Knows some javascript logics for modification.",
+//     contact_email: "javascript@gmail.com",
+//   };
+
+//   let { title, technologies, budget, description, contact_email } = data;
+//   Jobs.create({
+//     title,
+//     technologies,
+//     budget,
+//     description,
+//     contact_email,
+//   })
+//     .then((jobs) => res.redirect("/jobs"))
+//     .catch((err) => console.log(err));
 // });
 
 module.exports = router;
