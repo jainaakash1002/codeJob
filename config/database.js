59f896f5aba32986876c00830766ad66d4332469
Aakash Jain <jainaakash1002@gmail.com>
2023-04-16 23:23:04 +0530

    removed vercel and added testing database

--- config/database.js
+++ config/database.js
@@ -2,9 +2,23 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 
 let URL = "";
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+const {
+  PGHOST,
+  PGDATABASE,
+  PGUSER,
+  PGPASSWORD,
+  ENDPOINT_ID,
+  POSTGRESQL_ADDON_USER,
+  POSTGRESQL_ADDON_PASSWORD,
+  POSTGRESQL_ADDON_HOST,
+  POSTGRESQL_ADDON_PORT,
+  POSTGRESQL_ADDON_DB,
+} = process.env;
 
 switch (process.env.NODE_ENV) {
+  case "testing":
+    URL = `postgresql://${POSTGRESQL_ADDON_USER}:${POSTGRESQL_ADDON_PASSWORD}@${POSTGRESQL_ADDON_HOST}:${POSTGRESQL_ADDON_PORT}/${POSTGRESQL_ADDON_DB}`;
+    break;
   case "development":
     URL = `postgres://${PGUSER}:${PGPASSWORD}@${ENDPOINT_ID}.${PGHOST}/${PGDATABASE}?sslmode=require`;
     break;
